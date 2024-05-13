20c63df24d6942a19cb811dd2312d29e387201e3
juancazarezg <juanjose.cazarezg@gmail.com>
2024-05-12 20:11:58 -0600

    Nuevos cambios de static exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
-        unoptimized: true,
         domains: ['localhost', 'images.unsplash.com'],
     },
     eslint: {
